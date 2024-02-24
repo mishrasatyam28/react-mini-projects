ffda1b262b5ce694660b7b52a90cd4aed2518c15
Satyam Mishra <satyamsumit8750@gmail.com>
2024-02-24 23:01:25 +0530

    random color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import Accordian from "./components/accordian/Accordian";
+import RandomColor from "./components/random-color/RandomColor";
 
 function App() {
   return (
     <div>
       {/* Accordian component */}
       <Accordian />
+
+      {/* Random Color Component */}
+      <RandomColor />
     </div>
   );
 }
---
+++ src/components/random-color/RandomColor.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function RandomColor() {
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#123");
+
+  function handleCreateRandomColor() {}
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        background: color,
+        display: "grid",
+        justifyItems: "center",
+      }}
+    >
+      <div>
+        <button onClick={() => setTypeOfColor("hex")}>Create HEX Color</button>
+        <button onClick={() => setTypeOfColor("rgb")}>Create RGB Color</button>
+        <button onClick={handleCreateRandomColor}>Generate Random Color</button>
+      </div>
+    </div>
+  );
+}
