a5173f0a699efef0353961c67e30c4fb8291276c
Satyam Mishra <satyamsumit8750@gmail.com>
2024-03-02 23:26:26 +0530

    imageSlider

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Accordian from "./components/accordian/Accordian";
 import RandomColor from "./components/random-color/RandomColor";
 import StarRating from "./components/star-rating/StarRating";
+import ImageSlider from "./components/image-slider/ImageSlider";
 
 function App() {
   return (
@@ -14,6 +15,14 @@ function App() {
 
       {/* Star Rating */}
       <StarRating noOfStars={10} />
+
+      {/* Image slider component */}
+
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"4"}
+      />
     </div>
   );
 }
