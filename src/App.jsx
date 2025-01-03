36b64de11787f5e328c1ab28297ee8e214dcc534
Satyam Mishra <satyamsumit8750@gmail.com>
2025-01-03 22:40:09 +0530

    01Counter Proejct

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import Accordian from "./components/accordian/Accordian";
 import RandomColor from "./components/random-color/RandomColor";
 import StarRating from "./components/star-rating/StarRating";
 import ImageSlider from "./components/image-slider/ImageSlider";
+import Counter from "./components/01counter/Counter";
 
 function App() {
   return (
     <div>
+      {/* Counter */}
+      <Counter/>
       {/* Accordian component */}
       <Accordian />
 
---
+++ src/components/01counter/Counter.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import './style.css'
+const Counter = () => {
+    const [count, setCount] = useState(0)
+    const [incrementOrDecrement, setincrementOrDecrement] = useState(1);
+ 
+
+    function handleDecrement() {
+        setCount(count - Number(incrementOrDecrement))
+    }
+
+    function handleIncrement() {
+        setCount(count + Number(incrementOrDecrement))
+    }
+
+    function reset()
+    {
+        setCount(0);
+        setincrementOrDecrement(1)
+    }
+    
+  return (
+      <div className='container'>
+          <div className="sequence">
+              <p className='sr-number'>1</p>
+          </div>
+          <h1 className='title-counter'>Counter</h1>
+          <p>{count}</p>
+          <div className="flex-button">
+              <button onClick={handleDecrement} className='button'>-</button>
+              <button onClick={handleIncrement} className='button'>+</button>
+          </div>
+          <div className="flex-input">
+              <p>Increment/Decrement by</p>
+              <input type="text" name="" id="" value={incrementOrDecrement} onChange={(e)=>setincrementOrDecrement(e.target.value)} />
+          </div>
+          <div className="">
+          <button onClick={reset} className='button'>Reset</button>
+          </div>
+    </div>
+  )
+}
+
+export default Counter
