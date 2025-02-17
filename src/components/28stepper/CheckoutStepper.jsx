78cd6787c7c697cde6adc4fd934bba786dee7c4f
Satyam Mishra <satyamsumit8750@gmail.com>
2025-02-17 23:49:10 +0530

    working stepper

--- src/components/28stepper/CheckoutStepper.jsx
+++ src/components/28stepper/CheckoutStepper.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
-function CheckoutStepper({stepConfig=[]}) {
+function CheckoutStepper({ stepConfig = [] }) {
+    
+    if (!stepConfig.length) {
+        return <></>;
+    }
+
   return (
       <div className='stepper'>
           {
               stepConfig.map((step,index) => {
                   return (
                       <div key={step.name}>
-                          <div className="">{index+1}</div>
+                          <div className="step-number">{index+1}</div>
                           <div className="step-name">{step.name}</div>
                       </div>
                   )
